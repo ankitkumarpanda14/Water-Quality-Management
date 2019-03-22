e8ee4731f511ab356707a9e7f363638d26c0a47e
ankitkumarpanda14 <35422387+ankitkumarpanda14@users.noreply.github.com>
2019-03-22 18:26:22 +0530

    Update PHSensor.ino

--- PHSensor.ino
+++ PHSensor.ino
@@ -1,18 +1,14 @@
 #define SensorPin A0            
-#define Offset 0.00            
-#define LED 13
 #define samplingInterval 20
 #define printInterval 800
 #define ArrayLenth  40    
 int pHArray[ArrayLenth];  
 int pHArrayIndex=0;    
-void setup(void)
+void setup()
 {
-  pinMode(LED,OUTPUT);  
   Serial.begin(9600);  
- 
 }
-void loop(void)
+void loop()
 {
   static unsigned long samplingTime = millis();
   static unsigned long printTime = millis();
@@ -20,18 +16,21 @@ void loop(void)
   if(millis()-samplingTime > samplingInterval)
   {
       pHArray[pHArrayIndex++]=analogRead(SensorPin);
-      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
+      if(pHArrayIndex==ArrayLenth)
+      {
+        pHArrayIndex=0;
+      }
       voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      pHValue = 3.5*voltage+Offset;
+      
+      pHValue = 3.5*voltage;
+      
       samplingTime=millis();
   }
-  if(millis() - printTime > printInterval)   
+  if(millis() - printTime > printInterval)  
   {
-	      Serial.print("Voltage:");
-        Serial.print(voltage,2);
-        Serial.print("    pH value: ");
-      	Serial.println(pHValue,2);
-        printTime=millis();
+    Serial.print("pH value:");
+    Serial.println(pHValue,2);
+    printTime=millis();
   }
 }
 double avergearray(int* arr, int number)
@@ -40,34 +39,39 @@ double avergearray(int* arr, int number)
   int max,min;
   double avg;
   long amount=0;
-  if(number<=0)
-  {
-    Serial.println("Error number for the array to avraging!/n");
-    return 0;
-  }
-  if(number<5){   
+  if(number<5)
+  {  
     for(i=0;i<number;i++){
       amount+=arr[i];
     }
     avg = amount/number;
     return avg;
-  }else
+  }
+  else
   {
-    if(arr[0]<arr[1]){
+    if(arr[0]<arr[1])
+    {
       min = arr[0];max=arr[1];
     }
-    else{
+    else
+    {
       min=arr[1];max=arr[0];
     }
-    for(i=2;i<number;i++){
-      if(arr[i]<min){
+    for(i=2;i<number;i++)
+    {
+      if(arr[i]<min)
+      {
         amount+=min;        //arr<min
         min=arr[i];
-      }else {
-        if(arr[i]>max){
+      }
+      else {
+        if(arr[i]>max)
+        {
           amount+=max;    //arr>max
           max=arr[i];
-        }else{
+        }
+        else
+        {
           amount+=arr[i]; //min<=arr<=max
         }
       }
