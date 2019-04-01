2c3972bccb94d8fe9fde9566afb7f962df53d3b4
ankitkumarpanda14 <35422387+ankitkumarpanda14@users.noreply.github.com>
2019-04-02 01:00:25 +0530

    Update ComboSensor.ino

--- ComboSensor.ino
+++ ComboSensor.ino
@@ -11,15 +11,16 @@ void setup()
 void loop()
 {
   volt= 0;
-  for(int i=0;i<40;i++)
+  for(int i=0;i<200;i++)
   {
     volt+= (((float)analogRead(Tpin)*5)/1023);
-    volt1+=(((float)analogRead(pHpin)*5)/1023);
+    volt1+= (((float)analogRead(pHpin)*5)/1023);
   }
   volt = volt/200;
-  volt1= volt1/200;
   volt = round_to_dp(volt,1);
+  volt1 = volt1/200;
   volt1 = round_to_dp(volt1,1);
+  
   if(volt<2.5)
   {
     ntu = 3000;
@@ -29,11 +30,11 @@ void loop()
   ntu = -1120.4*square(volt)+5742.3*volt-4353.1;
   }
   pH = 3.5*volt1;
-  Serial.print(pH);
-  Serial.println();
+  Serial.println(pH);
   delay(1000);
-  Serial.print(ntu);
-  Serial.println();
+  //Serial.println("V");
+  Serial.println(ntu);
+  //Serial.println("NTU");
   delay(1000);  
 }
 
