bfe65c2420532c18f3ce7c269d7d18c63e229504
ankitkumarpanda14 <35422387+ankitkumarpanda14@users.noreply.github.com>
2019-04-02 00:58:41 +0530

    Update RpiFireBase.py

--- RpiFireBase.py
+++ RpiFireBase.py
@@ -1,28 +1,34 @@
 def upload():
         from datetime import datetime
-        import time
-        from firebase import firebase
-        import serial
-        ArduinoSerial = serial.Serial("/dev/ttyACM0",9600)
-        time.sleep(2)
+	import time
+	from firebase import firebase
+	import serial
+	ArduinoSerial = serial.Serial("/dev/ttyACM0",9600)
+	time.sleep(2)
 
         now = str(datetime.now())
 
-            time.sleep(5)
-            pH = ArduinoSerial.readline()
-            time.sleep(5)
+    	time.sleep(5)
+    	pH = ArduinoSerial.readline()
+    	time.sleep(5)
 
-            ntu = ArduinoSerial.readline()
-            time.sleep(5)
+    	ntu = ArduinoSerial.readline()
+    	time.sleep(5)
+    	
+    	#Alert Case
+    	alert = "NO"
+    	alert_ntu = float(ntu.strip())
+    	if(alert_ntu >= 2900):
+            alert = "YES"
 
-            host_name = 'https://water-quality-management-bb2ab.firebaseio.com/'
-        project_name = 'Water Quality Management'
-        time.sleep(1)
+    	host_name = 'https://water-quality-management-bb2ab.firebaseio.com/'
+	project_name = 'Water Quality Management'
+	time.sleep(1)
 
-          firebase= firebase.FirebaseApplication(host_name, authentication = None)
+   	firebase= firebase.FirebaseApplication(host_name, authentication = None)
 
 
-          result = firebase.post(project_name,{'Time:': now, 'pH:':str(pH), 'Turbidity:': str(ntu)})
-            print result
-            print "successful!!"
+   	result = firebase.post(project_name,{'Time': now, 'pH':str(pH), 'Turbidity': str(ntu), 'Alert':alert})
+    	print result
+    	print "Successful!!"
 
